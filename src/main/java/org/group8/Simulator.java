1b7a552f0535711d1e73883c8ec86cf9ec12e293
acervatus <142780750+acervatus@users.noreply.github.com>
2024-09-26 11:42:39 +0300

    mvc healthcare center

--- src/main/java/org/group8/Simulator.java
+++ src/main/java/org/group8/Simulator.java
@@ -3,13 +3,14 @@ package org.group8;
 import org.group8.framework.AbstractHealthCentre;
 import org.group8.framework.Trace;
 import org.group8.model.HealthCentre;
-
+import org.group8.view.HealthcenterGUI;
 public class Simulator {
-
+    
     public static void main(String[] args) {
         Trace.setTraceLevel(Trace.Level.INFO);
-        AbstractHealthCentre hc = new HealthCentre();
-        hc.setSimulationTime(1000);
-        hc.run();
+        HealthcenterGUI.launch(HealthcenterGUI.class);
+        //AbstractHealthCentre hc = new HealthCentre();
+        //hc.setSimulationTime(1000);
+        //hc.run();
     }
 }
