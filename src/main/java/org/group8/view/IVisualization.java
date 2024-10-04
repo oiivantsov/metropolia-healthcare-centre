b51f4e1147a90e9a0e60002ee94006de13e7b8fa
oiivantsov <oiivantsov@outlook.com>
2024-10-04 12:30:50 +0300

    add simulation status label

--- src/main/java/org/group8/view/IVisualization.java
+++ src/main/java/org/group8/view/IVisualization.java
@@ -4,5 +4,6 @@ public interface IVisualization {
 
     void clearDisplay();  // Method to clear the display
     void newPatient(String emoji);   // Method to add a new customer to the visualization
+    void removePatient();   // Method to remove a customer from the visualization
 
 }
